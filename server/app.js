06a09c78cdcdd2b105800faa4ab139c2e9fcc4e9
parjani <sangvikarparjani111@gmail.com>
2026-07-25 22:10:09 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -13,13 +13,15 @@ import memberDashboardRoutes from "./routes/memberDashboardRoutes.js";
 
 const app = express();
 
-app.use(cors());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
+
+app.use(express.json());
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
